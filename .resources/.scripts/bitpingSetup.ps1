41d0cae4247dea2c04c342949b2d57d29e0e0046
Incased Development <64618520+IncasedDevelopment@users.noreply.github.com>
2024-02-11 23:13:56 +1300

    optimization#224

--- .resources/.scripts/bitpingSetup.ps1
+++ .resources/.scripts/bitpingSetup.ps1
@@ -1,12 +1,17 @@
-#!/bin/pwsh
-Write-Output "First of all make sure that you have already run the runme.ps1 script and that you have registered to the various apps with the links provided."
-Read-Host -prompt "if you have already performed these actions, press enter to continue"
-Write-Output "To configure this app we will need to start an interactive container in a new terminal (so Docker needs to be already installed)."
-Write-Output "Then when prompted enter your bitping email and password in it. Hit enter and then close it (or CTRL+C it) as we will not need it anymore"
-Read-Host -prompt "When ready to start, press enter to continue"
+Write-Output "First, ensure that you have run the runme.ps1 script and registered to the various apps using the provided links."
+Read-Host -prompt "If you have completed these actions, press Enter to continue."
+
+Write-Output "To configure this app, we need to start an interactive container in a new terminal. Ensure Docker is installed."
+Write-Output "Then, when prompted, enter your Bitping email and password. Close the terminal afterward."
+
+Read-Host -prompt "When ready to start, press Enter to continue."
+
 docker run --rm -it -v ${PWD}/.data/.bitping/:/root/.bitping bitping/bitping-node:latest ;
+
 if ($LASTEXITCODE -eq 0) {
-    Write-Output "ok"
-  }
-Write-Output "Bitping interactive container closed. Bitping config should be complete now."
+    Write-Output "Bitping configuration completed."
+} else {
+    Write-Output "Error occurred during Bitping configuration."
+}
+
 Start-Sleep -Seconds 3
